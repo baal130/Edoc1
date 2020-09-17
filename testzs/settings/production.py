65d5b101450f698f63350dfdb3ae5cfe89794eaa
zoran <baal130@gmail.com>
2020-09-17 17:14:19 +0200

    production

--- testzs/settings/production.py
+++ testzs/settings/production.py
@@ -45,12 +45,21 @@ SERVICE_LIST = []
 # EMAIL_PORT=587
 # EMAIL_USE_TLS=False
 # DEFAULT_FROM_EMAIL = 'doctores@zoransostaric.com'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='traveldoctor@traveldoctor.mx'
-EMAIL_HOST_PASSWORD='a1b31734485'
+
+# EMAIL_HOST='smtp.gmail.com'
+# EMAIL_HOST_USER='traveldoctor@traveldoctor.mx'
+# EMAIL_HOST_PASSWORD='a1b31734485'
+# EMAIL_PORT=587
+# EMAIL_USE_TLS=True
+# DEFAULT_FROM_EMAIL = 'traveldoctor@traveldoctor.mx'
+
+EMAIL_HOST='smtp.sendgrid.net'
+EMAIL_HOST_USER='apikey'
+EMAIL_HOST_PASSWORD='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'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 DEFAULT_FROM_EMAIL = 'traveldoctor@traveldoctor.mx'
+
 
 
 # Application definition
@@ -137,8 +146,15 @@ MIDDLEWARE = (
 
 
 )
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# for HTTPS AND REDIRECTS TO HTTPS://www.
+CORS_REPLACE_HTTPS_REFERER      = True
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
 
 ROOT_URLCONF = 'testzs.urls'
 
